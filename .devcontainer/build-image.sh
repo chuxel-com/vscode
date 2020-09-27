1c1ae361c851285066f4bfcc05d5eb0050e9a01e
Chuck Lantz <clantz@microsoft.com>
2020-09-27 13:04:50 -0700

    Update dev container, disable CI

---
+++ .devcontainer/build-image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd $(dirname $0)
+REPOSITORY=${1:-"ghcr.io/chuxel-com/devcontainers/code-oss"}
+TAG=${2:-"latest"}
+docker build -t ${REPOSITORY}:${TAG} -f base.Dockerfile .
+docker push ${REPOSITORY}:${TAG}
+
