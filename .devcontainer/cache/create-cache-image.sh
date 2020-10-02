cf30d2cd8f3adfc2692ee7aceeffe077b11f7c46
Chuck Lantz <clantz@microsoft.com>
2020-10-02 02:48:31 +0000

    Fixes

--- .devcontainer/cache/create-cache-image.sh
+++ .devcontainer/cache/create-cache-image.sh
@@ -2,5 +2,5 @@
 cd $(dirname $0)
 TAG=${1:-"latest"}
 REPOSITORY=${2:-"ghcr.io/chuxel-com/microsoft/vscode/cache"}
-docker build -t ${REPOSITORY}:${TAG} -f precache.Dockerfile .
+docker build -t ${REPOSITORY}:${TAG} -f cache.Dockerfile .
 docker push ${REPOSITORY}:${TAG}
