542d1fb5e40ba90e74a4eab5f2c37563c4f8d9b6
Chuck Lantz <clantz@microsoft.com>
2020-10-03 03:58:59 +0000

    Fix

--- .devcontainer/cache/before-cache.sh
+++ .devcontainer/cache/before-cache.sh
@@ -3,5 +3,5 @@ set -e
 SOURCE_FOLDER="${1:-"."}"
 
 cd "${SOURCE_FOLDER}"
-find .  -not -path "./.git/*" > /tmp/devcontainer-cache-before.txt
+find .  -not -path "*./.git/*" -and -not -path "./.git" > /tmp/devcontainer-cache-before.txt
 
--- .devcontainer/cache/cache-diff.sh
+++ .devcontainer/cache/cache-diff.sh
@@ -8,10 +8,10 @@ CACHE_FOLDER=${2:-"/usr/local/etc/devcontainer-cache"}
 echo "[$(date)] Starting cache operation"
 
 cd "${SOURCE_FOLDER}"
-find .  -not -path "./.git/*" > /tmp/devcontainer-cache-after.txt
+find .  -not -path "*./.git/*" -and -not -path "./.git" > /tmp/devcontainer-cache-after.txt
 grep -Fxvf /tmp/devcontainer-cache-before.txt /tmp/devcontainer-cache-after.txt > /tmp/devcontainer-cache.manifest
 mkdir -p "${CACHE_FOLDER}"
-tar -cf "${CACHE_FOLDER}/cache.tar" -t /tmp/devcontainer-cache.manifest
+tar -cf "${CACHE_FOLDER}/cache.tar" --files-from /tmp/devcontainer-cache.manifest --totals
 du -h "${CACHE_FOLDER}/cache.tar"
 
 echo "[$(date)] Caching complete."
