3fbe3bebe216e680b60bd7b06af653b5df55a64f
Chuck Lantz <clantz@microsoft.com>
2020-12-03 02:17:07 +0000

    Fix restore diff

--- .devcontainer/cache/restore-diff.sh
+++ .devcontainer/cache/restore-diff.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-# This file contains the steps that should be run when creating the intermediary image that contains
-# contents for that should be in the image by default. It will be used to build up from the base image
-# to create an image that speeds up first time use of the dev container by "caching" the results
-# of these commands. Developers can still run these commands without an issue once the container is
-# up, but only differences will be processed which also speeds up the first time these operations occur.
+# This file restores the results of the "prepare.sh" into their proper locations
+# once the container has been created. It runs as a postCreateCommand which
+# in GitHub Codespaces occurs parallel to other startup activities and does not
+# really add to the overal startup time given how quick the operation ends up being.
 
-yarn install
-yarn electron
+set -e
+
+SOURCE_FOLDER="$(cd "${1:-"."}" && pwd)"
+CACHE_FOLDER="${2:-"/usr/local/etc/devcontainer-cache"}"
+
+if [ ! -d "${CACHE_FOLDER}" ]; then
+	echo "No cache folder found."
+	exit 0
+fi
+
+echo "[$(date)] Expanding $(du -h "${CACHE_FOLDER}/cache.tar") file to ${SOURCE_FOLDER}..."
+cd "${SOURCE_FOLDER}"
+tar -xf "${CACHE_FOLDER}/cache.tar"
+rm -f "${CACHE_FOLDER}/cache.tar"
+echo "[$(date)] Done!"
